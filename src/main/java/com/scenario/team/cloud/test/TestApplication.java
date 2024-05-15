c1030cbf5617f3aa2ee21a02439839fb83f7c2cd
Pedro Guilherme Alves <pedro@scenario.ind.br>
2024-05-15 12:03:59 -0300

    enable cors

--- src/main/java/com/scenario/team/cloud/test/TestApplication.java
+++ src/main/java/com/scenario/team/cloud/test/TestApplication.java
@@ -1,13 +1,28 @@
 package com.scenario.team.cloud.test;
 
+import jakarta.annotation.Nonnull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
 
+
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@Nonnull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*");
+            }
+        };
+    }
 }
